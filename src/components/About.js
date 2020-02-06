62eb07d42d9187c5a28497254da7bb5506bf3099
Matheus Henrique <matheus.181137@gmail.com>
2020-02-06 05:30:31 -0400

    responsive fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,7 @@
 import React from "react"
-import styled, { keyframes } from "styled-components"
+import styled from "styled-components"
 
 import Panel from "./Panel"
-
-const fadein = keyframes`
-  from {
-    opacity: 0%
-  }
-
-  to {
-    opacity: 100%;
-  }
-`
 
 const Container = styled.section`
   display: flex;
@@ -26,45 +16,34 @@ const Container = styled.section`
     line-height: 32px;
     width: 280px;
     margin-bottom: 40px;
-    margin-left: 22%;
     color: ${props => props.theme.primaryFontColor};
   }
 
-  @media only screen and (max-width: 1400px) {
+  @media only screen and (max-width: 950px) {
     padding: 45px 0;
     height: auto;
-    align-items: center;
-
-    h2 {
-      margin-left: 0;
-    }
-  }
-
-  @media only screen and (max-width: 600px) {
-    padding: 45px 0;
-    height: auto;
-
-    h2 {
-      margin-left: 5%;
-    }
   }
 `
 
 const Wraper = styled.div`
-  animation: ${fadein} 1s ease-in;
   display: flex;
-  align-items: center;
-  margin-left: 22%;
+  flex-direction: column;
+  align-items: flex-start;
+  max-width: 1160px;
+  margin: 0 auto;
 
-  @media only screen and (max-width: 1400px) {
+  @media only screen and (max-width: 950px) {
     flex-direction: column;
-    align-items: flex-end;
-    justify-content: space-around;
-    padding: 20px 0;
-    margin-left: 0%;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 0px;
   }
+`
 
-  @media only screen and (max-width: 600px) {
+const PanelWraper = styled.div`
+  display: flex;
+
+  @media only screen and (max-width: 950px) {
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
@@ -75,26 +54,28 @@ const Wraper = styled.div`
 function About() {
   return (
     <Container>
-      <h2>Como a Behind funciona?</h2>
       <Wraper>
-        <Panel
-          title={"1."}
-          text={
-            "Você se cadastra na Behind, e faz uma conexão neural com o seu sapato. É rápido e não precisa de nada além do seu celular."
-          }
-        />
-        <Panel
-          title={"2."}
-          text={
-            "O seu sapato recebe as ondas cerebrais e instantaneamente faz uma conexão neural. Essa conexão só pode ser quebrada por um poderoso jutsu."
-          }
-        />
-        <Panel
-          title={"3."}
-          text={
-            "Todo caminho tomado pelo seu sapato será monitorado pelo app da Behind. Monitoramos até mesmo em outras galáxias. Don't Panic!"
-          }
-        />
+        <h2>Como a Behind funciona?</h2>
+        <PanelWraper>
+          <Panel
+            title={"1."}
+            text={
+              "Você se cadastra na Behind, e faz uma conexão neural com o seu sapato. É rápido e não precisa de nada além do seu celular."
+            }
+          />
+          <Panel
+            title={"2."}
+            text={
+              "O seu sapato recebe as ondas cerebrais e instantaneamente faz uma conexão neural. Essa conexão só pode ser quebrada por um poderoso jutsu."
+            }
+          />
+          <Panel
+            title={"3."}
+            text={
+              "Todo caminho tomado pelo seu sapato será monitorado pelo app da Behind. Monitoramos até mesmo em outras galáxias. Don't Panic!"
+            }
+          />
+        </PanelWraper>
       </Wraper>
     </Container>
   )
